bae73038c323f5dd296cf21170f2d5b913786a5e
Khurram Shezad Sandhu <khurrami@672512.no-reply.drupal.org>
2018-05-16 15:48:57 +0500

    example.R updated

--- example.R
+++ example.R
@@ -1,18 +1,21 @@
 library(processx)
 library(rjobscheduler)
-
-enqueue(list(val='/home/jobs/job1.R'))
-enqueue(list(val='/home/jobs/job2.R'))
-enqueue(list(val='/home/jobs/job3.R'))
+#source('~/rjobscheduler/R/scheduler.R')
+Lsize <<- 0
+rear <<- new.env()
+Lqueue <<- new.env()
+enqueue(list(val='/home/khurram/jobs/job1.R'))
+enqueue(list(val='/home/khurram/jobs/job2.R'))
+enqueue(list(val='/home/khurram/jobs/job3.R'))
 
 count <- 1
-
+print(Lsize)
 while(!isEmptyQueue()) {
   ll <- bottom()
   procname <- paste0('p',count)
   cat('Process Name:',procname,"\n")
   filepath <- ll$val
-  Sys.sleep(10)
+  Sys.sleep(1)
   procname <- process$new("Rscript", filepath)
   print(procname)
   dequeue();
